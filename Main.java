c7cbd0cabf2a5a24ee55676e72c77fbf398ebfe6
MasterT0ad <121360137+MasterT0ad@users.noreply.github.com>
2024-01-07 11:05:34 -0300

    Main.java

--- Main.java
+++ Main.java
@@ -13,23 +13,20 @@ public class Main {
 
         println("Choose the type of temperature (fahrenheit, kelvin): ");
         String type = scanner.nextLine();
-        String fahrenheit = "fahrenheit";
-        String kelvin = "kelvin";
 
-            if (type.equals(fahrenheit)){
-                double fahrenheitTemp = ((temperature * 9/5) + 32);
-                println("The temperature in fahrenheit is: "+fahrenheitTemp);
+        if (type.equals("fahrenheit")){
+            double fahrenheitTemp = ((temperature * 9/5) + 32);
+            println("The temperature in fahrenheit is: "+fahrenheitTemp);
             }
         
-            if (type.equals(kelvin)){
+        if (type.equals("kelvin")){
+            if(temperature < (-273.15)){
+                println("There is no negative kelvin");
 
-                    if(temperature < (-273.15)){
-                    println("There is no negative kelvin");
+            } else {
 
-                    } else {
-
-                    double kelvinTemp = (temperature + 273.15);
-                    println("The temperature in kelvin is: "+kelvinTemp);
+                double kelvinTemp = (temperature + 273.15);
+                println("The temperature in kelvin is: "+kelvinTemp);
                     }
             }
     }
